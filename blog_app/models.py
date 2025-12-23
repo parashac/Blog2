2d34e76be5d2cb47fe3f3bc360c0066034d24a45
parashac <parashacharya139@gmail.com>
2025-12-23 17:15:09 +0545

    model

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class POST(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_one_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(null=True, blank =True)
+
+    def __str__(self):
+        return self.title()
