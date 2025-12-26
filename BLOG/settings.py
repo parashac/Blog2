0ce34b20f60604c5dbfd24113425c24d63063f64
parashac <parashacharya139@gmail.com>
2025-12-26 17:21:57 +0545

    login/logout

--- BLOG/settings.py
+++ BLOG/settings.py
@@ -117,8 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS =['static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
